8d84b90b6ac56ee5fb6a88134ccbf05162223784
SilviaP46 <61684236+SilviaP46@users.noreply.github.com>
2021-12-02 23:08:55 +0200

    added map

--- app/src/main/java/com/example/bsafecluj/MapPage.java
+++ app/src/main/java/com/example/bsafecluj/MapPage.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentFactory;
+
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapPage extends AppCompatActivity {
 
@@ -16,6 +20,13 @@ public class MapPage extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_main_page);
+
+        Fragment fragment=new MapFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout,fragment)
+                .commit();
 
     }
 }
